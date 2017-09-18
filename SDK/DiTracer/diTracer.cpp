41c1755cabe816b00b19beb17ec9d2103e455400
Vladislav Vulchev <vladislav.vulchev@chaosgroup.com>
2017-09-18 17:39:56 +0300

    Forgotten changes added

--- SDK/DiTracer/diTracer.cpp
+++ SDK/DiTracer/diTracer.cpp
@@ -12,6 +12,14 @@ const int BUFFER_HEIGHT = 768;
 
 void createContext(RTcontext* context, RTbuffer* outputBufferObj) {
 	RTvariable outputBuffer;
+	RTprogram rayGenerationProgram;
+
+	RTvariable eye;
+	RTvariable U;
+	RTvariable V;
+	RTvariable W;
+
+	char pathToPTX[512];
 
 	RT_CHECK_ERROR(rtContextCreate(context));
 	RT_CHECK_ERROR(rtContextSetRayTypeCount(*context, 1));
@@ -22,7 +30,35 @@ void createContext(RTcontext* context, RTbuffer* outputBufferObj) {
 	RT_CHECK_ERROR(rtBufferSetSize2D(*outputBufferObj, BUFFER_WIDTH, BUFFER_HEIGHT));
 	RT_CHECK_ERROR(rtVariableSetObject(outputBuffer, *outputBufferObj));
 
+	sprintf(pathToPTX, "%s%s", sutil::samplesDir(), "diTracer_generated_pinholeCamera.cu.ptx");
+	RT_CHECK_ERROR(rtProgramCreateFromPTXFile(*context, pathToPTX, "pinholeCamera", &rayGenerationProgram));
+	RT_CHECK_ERROR(rtContextSetRayGenerationProgram(*context, 0, rayGenerationProgram));
+	RT_CHECK_ERROR(rtContextDeclareVariable(*context, "eye", &eye));
+	RT_CHECK_ERROR(rtContextDeclareVariable(*context, "U", &U));
+	RT_CHECK_ERROR(rtContextDeclareVariable(*context, "V", &V));
+	RT_CHECK_ERROR(rtContextDeclareVariable(*context, "W", &W));
 
+	optix::float3 camEye = { .0f, .0f, 5.f };
+	optix::float3 lookat = { .0f, .0f, .0f };
+	optix::float3 up = { .0f, 1.f, .0f };
+	float hfov = 60.f;
+	float aspectRatio = (float)BUFFER_WIDTH / (float)BUFFER_HEIGHT;
+	optix::float3 cameraU, cameraV, cameraW;
+
+	sutil::calculateCameraVariables(
+		camEye, lookat, up, hfov, aspectRatio,
+		cameraU, cameraV, cameraW);
+
+	RT_CHECK_ERROR(rtVariableSet3fv(eye, &camEye.x));
+	RT_CHECK_ERROR(rtVariableSet3fv(U, &cameraU.x));
+	RT_CHECK_ERROR(rtVariableSet3fv(V, &cameraV.x));
+	RT_CHECK_ERROR(rtVariableSet3fv(W, &cameraW.x));
+
+	sprintf(pathToPTX, "%s/%s", sutil::samplesPTXDir(), "");
+	RT_CHECK_ERROR(rtProgramCreateFromPTXFile(*context, pathToPTX, "miss", &missProgram));
+	RT_CHECK_ERROR(rtProgramDeclareVariable(missProgram, "bgColor", &bgColor));
+	RT_CHECK_ERROR(rtVariableSet3f(bgColor, 1.f, .0f, .0f));
+	RT_CHECK_ERROR(rtContextSetMissProgram(*context, 0, missProgram));
 }
 
 //**************************************************************************************************************************
--- SDK/DiTracer/pinholeCamera.cu
+++ SDK/DiTracer/pinholeCamera.cu
@@ -1,3 +1,27 @@
 #include <optix_world.h>
 #include "helpers.h"
 
+using namespace optix;
+
+rtDeclareVariable(float3, eye, , );
+rtDeclareVariable(float3, U, , );
+rtDeclareVariable(float3, V, , );
+rtDeclareVariable(float3, W, , );
+rtDeclareVariable(rtObject, topObject, , );
+
+rtDeclareVariable(uint2, launchIndex, rtLaunchIndex, );
+rtDeclareVariable(uint2, launchDim, rtLaunchDim, );
+
+//**************************************************************************************************************************
+//**************************************************************************************************************************
+
+RT_PROGRAM void pinholeCamera() {
+	float2 d = make_float2(launchIndex) / make_float2(launchDim) * 2.f - 1.f;
+	float2 rayOrigin = eye;
+	float2 rayDirection = normalize(d.x * U + d.y * V + W);
+
+	optix::Ray ray = optix::make_Ray(rayOrigin, rayDirection, 0u, 1.e-4f, RT_DEFAULT_MAX);
+
+	Payload payload;
+	rtTrace(topObject, ray, payload);
+}
